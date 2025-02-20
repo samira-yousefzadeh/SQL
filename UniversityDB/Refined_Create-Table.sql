cd9ea4d843c56899ca2b2bd3511c6bb107a055c7
samira-shy <world.skc@gmail.com>
2025-02-20 21:42:46 +0000

    SQL
    create Table

--- UniversityDB/Refined_Create-Table.sql
+++ UniversityDB/Refined_Create-Table.sql
@@ -11,3 +11,29 @@ CREATE TABLE Department (
 );
 
 
+----
+
+CREATE TABLE Staff (
+    StaffID INT IDENTITY(1,1) PRIMARY KEY,
+    DepID INT FOREIGN KEY REFERENCES Department(DepID),
+    StaffName VARCHAR(100)
+);
+
+CREATE TABLE Faculty (
+    FacultyID INT IDENTITY(1,1) PRIMARY KEY,
+    FacultyName VARCHAR(100),
+    Description TEXT,
+    BuildingID INT FOREIGN KEY REFERENCES Building(BuildingID)
+);
+
+
+
+
+
+CREATE TABLE Student (
+    StudentID INT IDENTITY(1,1) PRIMARY KEY,
+    Gender VARCHAR(10),
+    Name VARCHAR(100),
+    Family VARCHAR(100),
+    Address TEXT
+);
