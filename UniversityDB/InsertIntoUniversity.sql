e2de9ef3d9c22a880b2df59e501a72b7d013e706
samira-shy <world.skc@gmail.com>
2025-02-21 23:19:05 +0000

    Alter Table- Add Column- Insertinto Table

--- UniversityDB/InsertIntoUniversity.sql
+++ UniversityDB/InsertIntoUniversity.sql
@@ -1,7 +1,20 @@
 -- Insert into Building
-INSERT INTO Building (Address, NumberOfRooms) VALUES
-('123 Main St', 10),
-('456 Elm St', 15),
-('789 Oak St', 12),
-('321 Pine St', 20),
-('654 Maple St', 18);
+INSERT INTO Building (BuildingLocation, NumberOfRooms,BuildingName,Campus) VALUES
+('Lansdowne', 10,'Bournemouth Gateway Building (BG)','Lansdowne'),
+('Lansdowne', 18,'Executive Business Centre (EB)','Lansdowne'),
+('Talbot', 20,'The Fusion Building (F)','Talbot'),
+('Talbot', 26,'Innovation Centre - Poole House (PG)	','Talbot'),
+('Talbot', 30,'Poole House (P)','Talbot'),
+('Talbot', 30,'Poole Gateway Building (PG)','Talbot'),
+('Talbot', 30,'The Sir Michael Cobham Library (DL)','Talbot');
+
+
+-- Insert into Faculty
+INSERT INTO Faculty (FacultyName, Description, BuildingID) VALUES
+('Media & Communication', 'Faculty of Media & Communication', 1),
+('Business', 'Faculty of Business & Economics', 2),
+('Science and Technology', 'Faculty of Science and Research', 3),
+('Health & Social Sciences', 'Faculty of Health & Social Sciences', 4);
+
+
+
