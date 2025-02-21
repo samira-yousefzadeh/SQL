033db395066a712d09e27e6b754f9fab4155df8d
samira-shy <world.skc@gmail.com>
2025-02-21 22:38:13 +0000

    SQL
    Insert Into

---
+++ UniversityDB/InsertIntoUniversity.sql
@@ -0,0 +1,7 @@
+-- Insert into Building
+INSERT INTO Building (Address, NumberOfRooms) VALUES
+('123 Main St', 10),
+('456 Elm St', 15),
+('789 Oak St', 12),
+('321 Pine St', 20),
+('654 Maple St', 18);
