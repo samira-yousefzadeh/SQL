eeaa68a1f00a94bb0a8fd95c49ec961ca5dfed31
samira-shy <world.skc@gmail.com>
2025-02-22 00:27:45 +0000

    SQL
    INSERT
    ALTER
    RESEED

--- UniversityDB/InsertIntoUniversity.sql
+++ UniversityDB/InsertIntoUniversity.sql
@@ -16,5 +16,19 @@ INSERT INTO Faculty (FacultyName, Description, BuildingID) VALUES
 ('Science and Technology', 'Faculty of Science and Research', 3),
 ('Health & Social Sciences', 'Faculty of Health & Social Sciences', 4);
 
-
-
+-- Insert into Department
+INSERT INTO Department (FacultyID, DepartmentName) VALUES
+(1, 'Accounting, Finance & Economics'),
+(1, 'People & Organisations'),
+(2, 'Marketing, Strategy & Innovation'),
+(3, 'Sport and Event Management'),
+(4, 'Medical Science & Public Health'),
+(5, 'Midwifery & Health Sciences'),
+(6, 'Nursing Science'),
+(7, 'Rehabilitation & Sport Sciences'),
+(8, 'Social Sciences & Social Work'),
+(9, 'Communication & Journalism'),
+(10, 'Humanities & Law '),
+(11, 'Media Production'),
+(12, 'National Centre for Computer Animation');
+ 
