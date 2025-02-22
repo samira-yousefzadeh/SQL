7f9ef57e2a72e0f3da257006f55f18ee2ef02d87
samira-shy <world.skc@gmail.com>
2025-02-22 00:29:21 +0000

    SQL Queries
    Reseed
    insert
    alter
    delete

--- UniversityDB/InsertIntoUniversity.sql
+++ UniversityDB/InsertIntoUniversity.sql
@@ -11,24 +11,29 @@ INSERT INTO Building (BuildingLocation, NumberOfRooms,BuildingName,Campus) VALUE
 
 -- Insert into Faculty
 INSERT INTO Faculty (FacultyName, Description, BuildingID) VALUES
-('Media & Communication', 'Faculty of Media & Communication', 1),
+('Media & Communication', 'Faculty of Media & Communication', 3),
 ('Business', 'Faculty of Business & Economics', 2),
-('Science and Technology', 'Faculty of Science and Research', 3),
-('Health & Social Sciences', 'Faculty of Health & Social Sciences', 4);
+('Science and Technology', 'Faculty of Science and Research', 5),
+('Health & Social Sciences', 'Faculty of Health & Social Sciences', 1);
 
 -- Insert into Department
 INSERT INTO Department (FacultyID, DepartmentName) VALUES
-(1, 'Accounting, Finance & Economics'),
-(1, 'People & Organisations'),
+(2, 'Accounting, Finance & Economics'),
+(2, 'People & Organisations'),
 (2, 'Marketing, Strategy & Innovation'),
-(3, 'Sport and Event Management'),
+(2, 'Sport and Event Management'),
 (4, 'Medical Science & Public Health'),
-(5, 'Midwifery & Health Sciences'),
-(6, 'Nursing Science'),
-(7, 'Rehabilitation & Sport Sciences'),
-(8, 'Social Sciences & Social Work'),
-(9, 'Communication & Journalism'),
-(10, 'Humanities & Law '),
-(11, 'Media Production'),
-(12, 'National Centre for Computer Animation');
- 
+(4, 'Midwifery & Health Sciences'),
+(4, 'Nursing Science'),
+(4, 'Rehabilitation & Sport Sciences'),
+(4, 'Social Sciences & Social Work'),
+(1, 'Communication & Journalism'),
+(1, 'Humanities & Law '),
+(1, 'Media Production'),
+(1, 'National Centre for Computer Animation'),
+(3, 'Archaeology & Anthropology'),
+(3, 'Computing & Informatics'),
+(3, 'Creative Technology'),
+(3, 'Design & Engineering'),
+(3, 'Psychology'),
+(3, 'Life & Environmental Sciences');
