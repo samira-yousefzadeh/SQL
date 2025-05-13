591a09fb718cdf7686e0fa7e6b53c5098a5b89bd
samira-shy <world.skc@gmail.com>
2025-05-13 15:10:26 +0100

    File Modification
    Removed unrelated files

--- UniversityDB/InsertIntoTable.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Student (FirstName, LastName, DateOfBirth, Email, PhoneNumber, Address, EnrollmentDate, Major, GPA)
-VALUES
-( 'John', 'Doe', '1998-05-12', 'john.doe@example.com', '123-456-7890', '123 Main St, City, Country', '2023-09-01', 'Computer Science', 3.8),
-('Jane', 'Smith', '2000-08-22', 'jane.smith@example.com', '987-654-3210', '456 Elm St, City, Country', '2023-09-01', 'Mathematics', 3.9);
